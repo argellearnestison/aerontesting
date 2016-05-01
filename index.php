14242a5b8da6bf584bede285388bf24e7ff5bc8c
Aeron Paul Ison <aeronpauldediosison@gmail.com>
2016-05-01 13:03:30 +0800

    started first project

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 <head>
 	<title>tangina</title>
 	<meta name="viewport" charset="utf-8" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/style.css" />
 </head>
 <body>
-	<!-- header included -->
-	<?php include('header.php'); ?>
+	<div class="page-wrapper">
+		<!-- header included -->
+		<?php include('header.php'); ?>
 
-	<!-- footer included -->
-	<?php include('footer.php'); ?>
+		<!-- footer included -->
+		<?php include('footer.php'); ?>
+	</div><!-- end of page-wrapper -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 </body>
 </html>
