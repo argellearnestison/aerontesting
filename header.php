86132e49d468e4ebac6fe2e725c23f240db7b239
Aeron Paul Ison <aeronpauldediosison@gmail.com>
2016-05-01 09:47:02 +0800

    started testing project

---
+++ header.php
@@ -0,0 +1,3 @@
+<header>
+	asdsadsa
+</header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>tangina</title>
+	<meta name="viewport" charset="utf-8" />
+</head>
+<body>
+	<!-- header included -->
+	<?php include('header.php'); ?>
+
+	<!-- footer included -->
+	<?php include('footer.php'); ?>
+</body>
+</html>
